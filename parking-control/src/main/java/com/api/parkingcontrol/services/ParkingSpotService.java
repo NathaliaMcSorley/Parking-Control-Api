208a16fb28b41736dd916c6797f23263012a9bc2
Nathalia <nathaliamcsorley@gmail.com>
2023-02-06 08:00:42 -0600

    Parking Spot API

---
+++ parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,56 @@
+package com.api.parkingcontrol.services;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ParkingSpotService {
+
+	// We are injecting with constructor but we could have used @autowired
+	final ParkingSpotRepository parkingSpotRepository;
+
+	public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+		this.parkingSpotRepository = parkingSpotRepository;
+	}
+
+	@Transactional
+	public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+		return parkingSpotRepository.save(parkingSpotModel);
+	}
+
+	public boolean existsByLicensePlateCar(String licensePlateCar) {
+		return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+	}
+
+	public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+		return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+	}
+
+	public boolean existsByApartmentAndBlock(String apartment, String block) {
+		return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+	}
+
+	public List<ParkingSpotModel> findAll() {
+		return parkingSpotRepository.findAll();
+	}
+
+	public List<ParkingSpotModel> findByParkingSpotNumber(String parkingSpotNumber) {
+		return parkingSpotRepository.findByParkingSpotNumber(parkingSpotNumber);
+	}
+
+	public Optional<ParkingSpotModel> findById(Integer id) {
+		return parkingSpotRepository.findById(id);
+	}
+
+	@Transactional
+	public void delete(ParkingSpotModel parkingSpotModel) {
+		parkingSpotRepository.delete(parkingSpotModel);
+	}
+}
